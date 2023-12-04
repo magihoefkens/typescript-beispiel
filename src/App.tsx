8cbd8fc5047b09e1cba0540c73eed914224fb1bd
magihoefkens <magi.hoefkens@outlook.de>
2023-12-04 15:23:46 +0100

    Neue Funktionen hinzugefügt

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,26 @@ function App() {
     if (isAdmin===false) {
             console.log("isAdmin has the value false");
         }
-
-
-
+    //Create an array of numbers. Use the '.map' function to double all the numbers in the array.
+    const numbers=[1,2,3,33,5];
+    const doubledNumbers=numbers
+        .map(num=>num*2);
+    console.log(doubledNumbers);
+    //Create an array of words. Use the '.filter' function to select only the words that are longer than 5 letters.
+    const words=["eins","zwei","sieben","dreiunddreißig"];
+    const wordsMore5Letters=words
+        .filter(w=>w.length>5);
+    console.log(wordsMore5Letters);
+    //Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.
+    const sum: number=numbers.reduce((a,b)=>a+b,0);
+    console.log(sum);
+    //Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.
+    const greaterThanTen:boolean=numbers.some((val)=>val>10);
+    if(greaterThanTen){
+        console.log("Zumindest eine Zahl ist größer als 10");
+    } else {
+        console.log("Keiner der Zahlen ist größer als 10");
+    }
   return (
     <>
 
